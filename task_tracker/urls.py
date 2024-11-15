c88d99fb23cbe801b022eff73c1ccf600b1f1771
sonya1045 <smikolaenko7@gmail.com>
2024-11-15 18:58:04 +0200

    update

--- task_tracker/urls.py
+++ task_tracker/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('<int:pk>/', views.TaskDetailView.as_view(), name='task-detail'),
     path('task-create', views.TaskCreateView.as_view(), name='task-create'),
     path('task-update/<int:pk>/', views.TaskUpdateView.as_view(), name='task-update'), 
-    path('comment-delete/<int:pk>/', views.CommentDeleteView.as_view(), name='task-delete'),
+    path('comment-delete/<int:pk>/', views.CommentDeleteView.as_view(), name='comment-delete'),
 ]   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
